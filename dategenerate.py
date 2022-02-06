f7009612a03dcd485d05ebd8eb31439052bdf406
pChitral <chitral.patil@vit.edu.in>
2022-02-06 21:45:36 +0530

    date generation for push workout

---
+++ dategenerate.py
@@ -0,0 +1,14 @@
+import datetime as dt
+import pandas as pd
+import numpy as np
+
+dates = [] #will load dates in it 
+
+for i in range(100):
+    start = dt.datetime.today()+dt.timedelta(i)
+    day = start.strftime("%A")
+    if (day == "Sunday" or day == "Thursday"):
+        dates.append(start.date().strftime("%d-%m-%Y"))
+
+s = pd.Series(np.array(dates))
+s.to_excel("Dates.xlsx")
