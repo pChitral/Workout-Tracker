ebd1a732ab494a7ac24e916a39fb09e89ab3b076
pChitral <chitral.patil@vit.edu.in>
2022-02-15 16:16:14 +0530

    Workout_Type and Workout_Subtype complete

--- database.py
+++ database.py
@@ -4,5 +4,10 @@ import sqlite3
 connection = sqlite3.connect('WorkoutTracker.db')
 cursor = connection.cursor()
 
-cursor.execute('CREATE TABLE EXERCISE(Exercise_ID INTEGER, Name TEXT, Workout_Subtype_ID INTEGER)')
+cursor.execute("INSERT INTO Workout_Subtype VALUES('2','1', 'Shoulder')")
+cursor.execute("INSERT INTO Workout_Subtype VALUES('3','1', 'Tricep')")
+cursor.execute("INSERT INTO Workout_Subtype VALUES('4','2', 'Back')")
+cursor.execute("INSERT INTO Workout_Subtype VALUES('5','2', 'Bicep')")
+cursor.execute("INSERT INTO Workout_Subtype VALUES('6','3', 'Legs')")
+
 connection.commit()
